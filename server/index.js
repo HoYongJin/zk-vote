3976e4d9679bb6566319814cf682ef127c64edb2
진호용 <jinhoyong@jinhoyongs-MacBook-Air.local>
2025-05-13 02:31:51 +0900

    prepare server for deploy

--- server/index.js
+++ server/index.js
@@ -10,13 +10,13 @@ const proofRouter = require("./routes/proof");
 const registerRouter = require("./routes/register");
 const registerByAdminRouter = require("./routes/registerByAdmin");
 const secretRouter = require("./routes/secret");
-const submitRouter = require("./routes/submit");
+//const submitRouter = require("./routes/submit");
 
 app.use("/proof", proofRouter);
 app.use("/register", registerRouter);
 app.use("/registerByAdmin", registerByAdminRouter);
 app.use("/secret", secretRouter);
-app.use("/submit", submitRouter);
+//app.use("/submit", submitRouter);
 
 app.listen(process.env.PORT, () => {
     console.log(`SERVER RUNNING ON http://localhost:${process.env.PORT}`);
