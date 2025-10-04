319d84967fa860671924d7185236d681ca99cb2d
진호용 <jinhoyong@jinhoyongs-MacBook-Air.local>
2025-10-04 23:30:36 +0900

    commit

--- server/index.js
+++ server/index.js
@@ -6,25 +6,25 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const proofRouter = require("./routes/proof");
-const registerRouter = require("./routes/register");
-const registerByAdminRouter = require("./routes/registerByAdmin");
-const secretRouter = require("./routes/secret");
-const submitZkRouter = require("./routes/submitZk");
-const addAdminsRouter = require("./routes/addAdmins");
 const setVoteRouter = require("./routes/setVote");
-const registerableVoteRouter = require("./routes/registerableVote");
-const finalizeVoteRouter = require("./routes/finalizeVote");
+// const proofRouter = require("./routes/proof");
+// const registerRouter = require("./routes/register");
+// const registerByAdminRouter = require("./routes/registerByAdmin");
+// const secretRouter = require("./routes/secret");
+// const submitZkRouter = require("./routes/submitZk");
+// const addAdminsRouter = require("./routes/addAdmins");
+// const registerableVoteRouter = require("./routes/registerableVote");
+// const finalizeVoteRouter = require("./routes/finalizeVote");
 
-app.use("/proof", proofRouter);
-app.use("/register", registerRouter);
-app.use("/registerByAdmin", registerByAdminRouter);
-app.use("/secret", secretRouter);
-app.use("/submitZk", submitZkRouter);
-app.use("/addAdmins", addAdminsRouter);
 app.use("/setVote", setVoteRouter);
-app.use("/registerableVote", registerableVoteRouter);
-app.use("/finalizeVote", finalizeVoteRouter);
+// app.use("/proof", proofRouter);
+// app.use("/register", registerRouter);
+// app.use("/registerByAdmin", registerByAdminRouter);
+// app.use("/secret", secretRouter);
+// app.use("/submitZk", submitZkRouter);
+// app.use("/addAdmins", addAdminsRouter);
+// app.use("/registerableVote", registerableVoteRouter);
+// app.use("/finalizeVote", finalizeVoteRouter);
 
 app.listen(process.env.PORT, () => {
     console.log(`SERVER RUNNING ON http://localhost:${process.env.PORT} & ${process.env.DEPLOY_URL}`);
