c13a4ac867a06a66d1a67ff28a66ca9b74e7fcd8
진호용 <jinhoyong@jinhoyongs-MacBook-Air.local>
2025-10-05 00:29:12 +0900

    commit

--- server/index.js
+++ server/index.js
@@ -10,21 +10,21 @@ const setVoteRouter = require("./routes/setVote");
 const registerableVoteRouter = require("./routes/registerableVote");
 const registerByAdminRouter = require("./routes/registerByAdmin");
 const registerRouter = require("./routes/register");
+const finalizeVoteRouter = require("./routes/finalizeVote");
 const proofRouter = require("./routes/proof");
 // const secretRouter = require("./routes/secret");
 // const submitZkRouter = require("./routes/submitZk");
 // const addAdminsRouter = require("./routes/addAdmins");
-// const finalizeVoteRouter = require("./routes/finalizeVote");
 
 app.use("/setVote", setVoteRouter);
 app.use("/registerableVote", registerableVoteRouter);
 app.use("/register", registerRouter);
 app.use("/registerByAdmin", registerByAdminRouter);
+app.use("/finalizeVote", finalizeVoteRouter);
 app.use("/proof", proofRouter);
 // app.use("/secret", secretRouter);
 // app.use("/submitZk", submitZkRouter);
 // app.use("/addAdmins", addAdminsRouter);
-// app.use("/finalizeVote", finalizeVoteRouter);
 
 app.listen(process.env.PORT, () => {
     console.log(`SERVER RUNNING ON http://localhost:${process.env.PORT} & ${process.env.DEPLOY_URL}`);
