18e8bef9f36626480625e92e21dd3039e57b06bd
진호용 <jinhoyong@jinhoyongs-MacBook-Air.local>
2025-10-07 22:31:16 +0900

    Remove frontend directory from repository

--- frontend/src/pages/AdminPage.js
+++
@@ -1,7 +0,0 @@
-// frontend/src/pages/LoginPage.js
-
-function AdminPage() {
-    return <div>AdminPage</div>;
-}
-
-export default AdminPage;
--- frontend/src/store/store.js
+++
@@ -1,9 +0,0 @@
-// frontend/src/store/store.js
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './authSlice';
-
-export const store = configureStore({
-  reducer: {
-    auth: authReducer, // 'auth' 라는 이름으로 authSlice를 등록
-  },
-});
