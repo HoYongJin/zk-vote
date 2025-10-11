e9f87b4a815aea3a6ef4324d267624ee5b011ad3
진호용 <jinhoyong@jinhoyongs-MacBook-Air.local>
2025-10-11 19:23:55 +0900

    commit

--- server/routes/registerByAdmin.js
+++ server/routes/registerByAdmin.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 const validator = require('validator');
 const supabase = require("../supabaseClient");
 const authAdmin = require("../middleware/authAdmin");
